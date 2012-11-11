d3a5e9bd306fb30d23e1026ab00dc9cf50c27d21
claudiay <yadathi@gmail.com>
2012-11-10 17:44:27 -0800

    YEEE HAAAWWW

--- question/views.py
+++ question/views.py
@@ -7,7 +7,7 @@ def home(request):
 	status = {
 		'comma': "Learn how to type! City COMMA State.",
 		'error': "I don't know what you did wrong. I can't find it.",
-		'sports': "Things are gonna suck because of a game.",
+		'sports': "Things are gonna suck because of a game in town.",
 		'maybe': "Probably, but not due to a sports game."
 	}
 
@@ -20,12 +20,12 @@ def home(request):
 
 				# Split the location in two, seperated with a comma.
 				location = cd['location'].split(',')
-				city = location[0]
-				state = location[1]
+				city = location[0].upper()
+				state = location[1].upper()
 
 				# Call the API with a request
-				r = requests.get('http://api.espn.com/v1/sports/events/venues?apikey=ngac5qbzrdxnxakjwb46fd3m')
-				things = json.loads(r.text)
+				if espn(city, state):
+					return render_to_response('index.html', {'status':status['sports']})
 				return render_to_response('index.html', {'status':status['maybe']})
 			else:
 				form = LocationForm()
@@ -35,3 +35,29 @@ def home(request):
 			return render(request, 'index.html', {'status':status['error'], 'form':form})
 	form = LocationForm()
 	return render(request, 'index.html', {'form':form})
+
+
+def espn(given_city, given_state):
+	r = requests.get('http://api.espn.com/v1/sports/events/venues?apikey=ngac5qbzrdxnxakjwb46fd3m')
+	nice = json.loads(r.text)
+
+
+	sports = nice['sports']
+
+	a_leagues = ['NBA', 'NFL', 'MLB', 'NHL']
+	l_events = []
+	for each in sports:
+		for name in each['leagues']:
+
+			if name['shortName'] in a_leagues:
+				temp = each['leagues']
+				l_events.append(each['leagues'])
+
+	for league in l_events:
+		for thing in league:
+			events = thing['events']
+			for event in events:
+				city = event['venues'][0]['city']
+				if city == given_city:
+					return True
+	return False
