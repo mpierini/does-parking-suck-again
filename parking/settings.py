e4e43977d7965fc6778e3b046941a0612c8fcac1
Michele <pierini.michele@gmail.com>
2013-02-18 22:48:19 -0800

    adding static dir name

--- parking/settings.py
+++ parking/settings.py
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = ROOTDIR + 'parking/static'
 #took out pre-existing dir name
 
 # URL prefix for static files.
