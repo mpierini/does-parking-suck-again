b3a7846d440dee21adcd33c2710e167373049027
Michele <pierini.michele@gmail.com>
2013-02-18 22:21:22 -0800

    removed dir names

--- parking/settings.py
+++ parking/settings.py
@@ -56,7 +56,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/michele/Desktop/parkr/sucks/doesparkingsuck/parking/static/'
+STATIC_ROOT = ''
+#took out pre-existing dir name
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -106,7 +107,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/michele/Desktop/parkr/sucks/doesparkingsuck/parking/templates',
+    '',
+    #took out pre-existing dir name
 )
 
 INSTALLED_APPS = (
